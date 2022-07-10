25ec5fb4785eb82e8697102987395157fd7d5d56
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-10 11:52:38 +0900

    주말이면 금요일 마감가격 볼 수 있게함.

--- stockreading/stockreading.py
+++ stockreading/stockreading.py
@@ -2,25 +2,27 @@ import FinanceDataReader as fdr
 from datetime import date
 import datetime
 today = date.today()
+today_wd = today.weekday()
 
 weekend = ''
 
-if today.weekday() == 5:
+if today_wd == 5:
     today -= datetime.timedelta(days=1)
     weekend = '토요일'
-elif today.weekday() == 6:
+elif today_wd == 6:
     today -= datetime.timedelta(days=2)
     weekend = '일요일'
 
 warning = f'오늘은 {weekend}이므로 금요일 기준으로 제공합니다.'
 
+#코스피 딕셔너리
 kospi_dict = dict()
 kospi_dict['SK하이닉스'] = '000660'
 kospi_dict['삼성전자'] = '005930'
 kospi_dict['삼성전자우'] = '005935'
 kospi_dict['kodex2차전지'] = '305720'
 
-
+# 나스닥 딕셔너리
 nasdaq_dict = dict()
 nasdaq_dict['존슨앤존슨'] = 'JNJ'
 nasdaq_dict['코카콜라'] = 'KO'
@@ -33,13 +35,23 @@ today_str = today.strftime('%Y-%m-%d')
 result_str = ''
 result_str += '='*10 + '코스피' + '='*10
 
+
 for key, val in zip(kospi_dict.keys(), kospi_dict.values()):
-    open_price = fdr.DataReader(val, today).loc[today_str, 'Open']
-    result_str += f'\n\n {key} \n 시작가격 :  {int(open_price)}원'
+    #주말일 경우 금요일 마감 가격을 보여주기
+    if today_wd >= 5:
+        open_price = fdr.DataReader(val, today).loc[today_str, 'Close']
+        result_str += f'\n\n {key} \n 마감가격 :  {int(open_price)}원'
+    else:
+        open_price = fdr.DataReader(val, today).loc[today_str, 'Open']
+        result_str += f'\n\n {key} \n 시작가격 :  {int(open_price)}원'
 
 result_str += '\n' + '='*10 + '나스닥' + '='*10
 for key, val in zip(nasdaq_dict.keys(), nasdaq_dict.values()):
-    open_price = fdr.DataReader(val, today).loc[today_str, 'Open']
-    result_str += f'\n\n {key} \n 시작가격 :  {float(open_price)}달러'
+    if today_wd >= 5:
+        open_price = fdr.DataReader(val, today).loc[today_str, 'Close']
+        result_str += f'\n\n {key} \n 마감가격 :  {float(open_price)}달러'
+    else:
+        open_price = fdr.DataReader(val, today).loc[today_str, 'Open']
+        result_str += f'\n\n {key} \n 시작가격 :  {float(open_price)}달러'
 
 print(result_str)
